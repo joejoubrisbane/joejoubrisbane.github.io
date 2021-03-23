0533a9566bebae8b2db2fcb38150de8efed1a516
Joe Jou <pk0204dg888@gmail.com>
2021-03-23 12:25:46 +1000

    add new html

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,10 +220,6 @@
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
